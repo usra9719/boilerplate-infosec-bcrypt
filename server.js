e8f20ec495c8e2c2b13c0a01741898465ae34c81
Yousra Hammouda <you13.ham@gmail.com>
2026-06-02 13:46:23 +0000

    fix: add sync bcrypt

--- server.js
+++ server.js
@@ -26,11 +26,13 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 });
 //END_ASYNC
 
-//END_ASYNC
+
 
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
